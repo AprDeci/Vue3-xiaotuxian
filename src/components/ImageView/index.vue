cdd39eaa61512c6c138640f1567692a9cc48ca2b
aprd <1263868407@qq.com>
2023-10-12 18:40:02 +0800

    放大镜效果

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from 'vue'
+import { useMouseInElement, watchArray } from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -15,6 +16,33 @@ const activeIndex = ref(0)
 const enterhandler=(i)=>{
   activeIndex.value=i
 }
+
+//获取鼠标相对位置
+const positionX=ref(0)
+const positionY=ref(0)
+const target = ref(null)
+const {elementX,elementY,isOutside} =useMouseInElement(target)
+
+const left =ref(0)
+const top = ref(0)
+
+watchArray([elementX,elementY],()=>{
+  console.log('xy变化了')
+  if(elementX.value>100&&elementY.value<300){
+    left.value=elementX.value-100
+  }
+  if(elementY.value>100&&elementX.value<300){
+    top.value=elementY.value-100
+  }
+  if(elementX.value>300) {left.value=200}
+  if(elementY.value>300) {top.value=200}
+  if(elementX.value<100) {left.value=0}
+  if(elementY.value<100) {top.value=0}
+  //控制大图显示
+positionX.value = -left.value * 2
+positionY.value = -top.value * 2
+})
+
 </script>
 
 
@@ -24,7 +52,7 @@ const enterhandler=(i)=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -35,11 +63,11 @@ const enterhandler=(i)=>{
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
