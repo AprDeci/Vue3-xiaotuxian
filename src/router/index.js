9c7d8305fdbe9e2809ed1e6dc3d32b718725300a
aprd <1263868407@qq.com>
2023-10-12 22:25:20 +0800

    表单验证

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Details from '@/views/Details/index.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
