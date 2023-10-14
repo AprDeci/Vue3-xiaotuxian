7ceb5019a33c571f16335fb77455355c5cdc9f28
aprd <1263868407@qq.com>
2023-10-14 17:21:16 +0800

    购物车完毕

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -2,11 +2,25 @@
 
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
+import { useUserStore } from "@/stores/user";
+import {insertCartAPI,findNewCartListAPI,delCartAPI} from '@/apis/cart'
 export const useCartStore = defineStore('cart',()=>{
+    const userStore = useUserStore()
+    const isLogin = computed(()=>userStore.userInfo.token)
     //1.定义state
     const cartList = ref([])
     //定义action 
-    const addCart = (goods)=>{
+    const updateNewList = async()=>{
+        const res= await findNewCartListAPI()
+         cartList.value = res.result
+    }
+    const addCart = async(goods)=>{
+        const {skuId,count} = goods
+        if(isLogin.value){
+         await insertCartAPI({skuId,count})
+         updateNewList()
+        }
+        else{
      //添加购物车操作
         //已添加过
         //没添加过
@@ -16,10 +30,16 @@ export const useCartStore = defineStore('cart',()=>{
     }else{
         cartList.value.push(goods)
     }
+}
     }
-    const delCart = (skuId)=>{
+    const delCart = async(skuId)=>{
+        if(isLogin.value){
+           await delCartAPI([skuId])
+           updateNewList()
+        }else{
         const item= cartList.value.findIndex((item)=>item.skuId===skuId)
         cartList.value.splice(item,1)
+    }
     }
 
     //计算属性
@@ -50,6 +70,10 @@ export const useCartStore = defineStore('cart',()=>{
             item.selected = selected
         })
     }
+    //清楚购物车
+    const clearCart = ()=>{
+        cartList.value=[]
+    }
     return{
         cartList,
         addCart,
@@ -60,7 +84,9 @@ export const useCartStore = defineStore('cart',()=>{
         isAll,
         allCheck,
         selectedCount,
-        selectedPrice
+        selectedPrice,
+        clearCart,
+        updateNewList
     }},
     {
         persist:true,
